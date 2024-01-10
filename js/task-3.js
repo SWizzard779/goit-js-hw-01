ed841a8265c0155be20493d68d60235bd4602e21
SWizzard779 <sofia.nesimko@gmail.com>
2024-01-10 13:54:53 +0100

    Homework_1

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,12 @@
 function getElementWidth(content, padding, border) {
-Number.parseFloat
-    return content + (padding*2) + (border*2);
+    const elementContent = Number.parseFloat(content);
+    const elementPadding = Number.parseFloat(padding);
+    const elementBorder = Number.parseFloat(border);
+
+    const elementWidth = elementContent + elementPadding*2 + elementBorder*2;
+    return elementWidth
 }
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-
